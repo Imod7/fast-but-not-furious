726e3907f83a6b5c17da34d7728887a2eb690358
Imod7 <dsaripapazoglou@gmail.com>
2022-06-01 13:01:57 +0200

    Added Chrome version & gif - Added a script for opening the tabs in chrome - Added a gif in the README

--- README.md
+++ README.md
@@ -3,9 +3,13 @@ Script that executes multiple searches at once by opening automatically a new fi
 
 ## Run script
 1. Open a terminal window
-2. `git clone` this repo
-3. `cd fast-but-not-furious`
-4. `sh fast-but-not-furious.sh keyword1 keyword2`
+2. `> git clone https://github.com/Imod7/fast-but-not-furious.git`
+3. `> cd fast-but-not-furious`
+4. Before running the commands below, please replace `keyword1` and `keyword2` with the keywords you are looking for.
+   ### Run in Firefox
+   4.1 `> sh fast-but-not-furious-firefox.sh keyword1 keyword2`
+   ### Run in Chrome
+   4.2 `> sh fast-but-not-furious-chrome.sh keyword1 keyword2`
 
 Note : You can also search for a combination of keywords as follows : 
 `sh fast-but-not-furious.sh keyword+keyword2`
@@ -22,3 +26,6 @@ by changing the contents of the arrays defined in the beginning of the script.
 
 ### A screenshot from running the script on the terminal
 ![fast-not-furious-script-screenshot](/fast_not_furious.png)
+
+### Short Walkthrough of running the script in firefox(gif)
+![Recording](fast-not-furious-firefox.gif "Short recording from the running the script")
---
+++ fast-but-not-furious-firefox.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+BLUE="\x1b[38;2;40;177;249m"
+GREEN="\x1b[38;2;80;200;120m"
+YELLOW="\x1b[38;2;253;253;150m"
+RESET="\e[0m"
+
+# To implement flags like : 
+# 1. flag -o  : Search in other blockchains eg. ethereum 
+# 2. flag -rc : relaychain -  only Polkadot related searches
+# 3. flag -p  : parachains - executes also parachain related searches
+# Add Search for 
+# Polkadot Watercooler element room
+# Parachain Technical
+# Substrate Technical (Public)
+
+
+printf $YELLOW
+cat ascii.txt 
+printf $RESET
+
+i=0
+for var in "$@"
+do
+  KEYWORDS[$i]=$var
+  i=$(($i+1))
+done
+
+# Define the organizations
+ORGS=('paritytech' 'polkadot-js' 'substrate-developer-hub')
+# Repos in the Parity tech org
+PARITY_TECH=('substrate' 'polkadot' 'txwrapper-core' 'substrate-api-sidecar')
+# Repos to search in the Substrate Developer Hub org
+DEVHUB=('substrate-docs' 'awesome-substrate')
+# When searching on the web, include also this keyword to specify the search
+WEB=('substrate' 'polkadot+wiki' 'polkadot-js' 'substrate+dev+hub')
+
+
+/Applications/Firefox.app/Contents/MacOS/firefox --new-window
+
+printf "\n$BLUE Searching in Github Repos $RESET \n"
+
+for org in "${ORGS[@]}";
+do
+  printf "$GREEN  Org : $org $RESET \n"
+  # Searching in Parity Tech Repos
+  if [[ "$org" == "paritytech" ]]; then
+    for repo in "${PARITY_TECH[@]}";
+    do
+      for keyword in "${KEYWORDS[@]}";
+      do
+        argsRepos="https://github.com/$org/$repo/search?q=$keyword"
+        sleep 0.5
+        /Applications/Firefox.app/Contents/MacOS/firefox --new-tab $argsRepos
+        printf "   $argsRepos \n"
+      done
+    done
+  fi
+  # Searching in Polkadot JS Repos
+  if [[ "$org" == "polkadot-js" ]]; then
+    for keyword in "${KEYWORDS[@]}";
+    do
+      argsRepos="https://github.com/$org/api/search?q=$keyword"
+      sleep 0.5
+      /Applications/Firefox.app/Contents/MacOS/firefox --new-tab $argsRepos
+      printf "   $argsRepos \n"
+    done
+  fi
+  # Searching in Substrate Developer Hub Repos
+  if [[ "$org" == "substrate-developer-hub" ]]; then
+    for repo in "${DEVHUB[@]}";
+    do
+      for keyword in "${KEYWORDS[@]}";
+      do
+        argsRepos="https://github.com/$org/$repo/search?q=$keyword"
+        sleep 0.5
+        /Applications/Firefox.app/Contents/MacOS/firefox --new-tab $argsRepos
+        printf "   $argsRepos \n"
+      done
+    done
+  fi
+done
+
+# Searching in the web
+printf "\n$BLUE Searching in the WEB $RESET \n"
+for search in "${WEB[@]}";
+do
+  for keyword in "${KEYWORDS[@]}";
+  do
+    argsSearchs="https://duckduckgo.com/?q=$search+$keyword"
+    sleep 0.5
+    /Applications/Firefox.app/Contents/MacOS/firefox --new-tab $argsSearchs
+    printf "  $argsSearchs \n"
+  done
+done
+
+# Searching in Substrate Stack Exchange
+printf "\n$BLUE Searching in Substrate Stack Exchange $RESET \n"
+for keyword in "${KEYWORDS[@]}";
+do
+  argsSearchs="https://substrate.stackexchange.com/search?q=$keyword"
+  sleep 0.5
+  /Applications/Firefox.app/Contents/MacOS/firefox --new-tab $argsSearchs
+  printf "  $argsSearchs \n"
+done
+
+# Searching in Parity's Forum
+printf "\n$BLUE Searching in Parity's Forum $RESET \n"
+for keyword in "${KEYWORDS[@]}";
+do
+  argsSearchs="https://forum.parity.io/search?q=$keyword"
+  sleep 0.5
+  /Applications/Firefox.app/Contents/MacOS/firefox --new-tab $argsSearchs
+  printf "  $argsSearchs \n"
+done
+
+
